eca01dde15745a52ed311e63e1c1b9eeb1a67644
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-22 09:20:40 +0200

    Comienzo del cloud config server

---
+++ src/main/java/primerCloudConfig/CloudConfigServer1/CloudConfigServer1Application.java
@@ -0,0 +1,13 @@
+package primerCloudConfig.CloudConfigServer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/primerCloudConfig/CloudConfigServer1/CloudConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package primerCloudConfig.CloudConfigServer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
